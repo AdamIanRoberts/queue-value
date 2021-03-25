22e34cfc770ae575c26b19c7ea62439ea2cf7471
AdamIanRoberts <adamianroberts@gmail.com>
2021-03-25 17:43:20 +0000

    Remove abstract method time_steps

--- queue-value/orderbook_aggregator/OrderbookAggregator.py
+++ queue-value/orderbook_aggregator/OrderbookAggregator.py
@@ -1,13 +1,7 @@
 import abc
-import numpy as np
 
 
 class OrderbookAggregator(metaclass=abc.ABCMeta):
-    @property
-    @abc.abstractmethod
-    def time_steps(self) -> list:
-        pass
-
     @property
     @abc.abstractmethod
     def orderbook_states(self) -> list:
